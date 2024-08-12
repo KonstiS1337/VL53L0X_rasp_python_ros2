8dafe19cc6a42cb40b08fc8adf285e5c79368ece
Konstantin Stute <konstantin.stute@stud.tu-darmstadt.de>
2024-08-12 16:24:42 +0200

    added first version of tof sensor ros2 node

---
+++ python/VL53L0X_ros2.py
@@ -0,0 +1,58 @@
+import VL53L0X
+import rclpy
+from rclpy.node import Node
+import rclpy.publisher
+from std_msgs.msg import Int16
+from rcl_interfaces.msg import SetParametersResult
+
+class TOFRosPublisher(Node) :
+
+    def __init__(self):
+        super().__init__('tof_ros_publisher_node')
+
+        self.declare_parameter("run",True)
+        self.run = True
+        self.add_on_set_parameters_callback(self.parameter_callback)
+
+        self.tof = VL53L0X.VL53L0X()
+        self.tof.open()
+        self.accuracy_mode = VL53L0X.Vl53l0xAccuracyMode.BETTER # change mode here if you want to use different accuracy modes
+        self.tof.start_ranging(self.accuracy_mode)
+        self.pub = self.create_publisher(Int16,self.get_parameter("epuck-name").get_parameter_value().string_value + '/tof',1)
+        self.timer = self.create_timer(0.1,self.cb)
+
+    def cb(self):
+        distance = self.tof.get_distance()
+        msg = Int16()
+        msg._data = distance
+        self.pub.publish(msg)
+    
+    def stop(self):
+        self.tof.stop_ranging()
+    
+    def clean(self):
+        self.tof.stop_ranging()
+        self.tof.close()
+    
+    def parameter_callback(self, params) -> None:
+        for param in params:
+            if(param.name == "run"):
+                if(not param.value and self.run): self.stop()
+                elif(param.value and not self.run): self.tof.start_ranging(self.accuracy_mode)
+
+        return SetParametersResult(successful=True)
+    
+def main(args=None):
+    rclpy.init(args=args)
+    node = TOFRosPublisher()
+    rclpy.spin(node)
+    node.clean()
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
+    
+    
